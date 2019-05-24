b0c953e2cb64ef9cb58a21db3113b471d19d6703
Vuk <vukovic.dev@gmail.com>
2019-05-25 00:22:01 +0200

    Started using VS Code

---
+++ batch/cmp.bat
@@ -0,0 +1,4 @@
+@echo off
+cd c:\bc31\bin
+bcc +C:\bc31\BIN\OS1Proj\batch\cmpsw.txt %1 APPLICAT.LIB
+@echo on
---
+++ batch/dbg.bat
@@ -0,0 +1,4 @@
+@echo off
+cd c:\bc31\bin
+td.exe -ctdconfig.td -sdC:\bc31\BIN\OS1Proj\src C:\bc31\BIN\OS1Proj\output\vs_test.exe
+@echo on
