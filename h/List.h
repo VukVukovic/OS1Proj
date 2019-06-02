58197a5c4f0c2a6ccddcc6335b38f71d5a587c2a
Vuk <vukovic.dev@gmail.com>
2019-06-02 17:23:09 +0200

    Added new list

--- h/List.h
+++ h/List.h
@@ -1,43 +1,103 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include "Util.h"
-
 template<class T>
 class List {
-protected:
-	struct Node{
+public:
+	struct Elem {
 		T data;
-		Node* next;
-
-		Node(T data, Node *next=nullptr) : data(data), next(next) {}
+		Elem *next, *prev;
+		Elem(T data) : data(data), prev(nullptr), next(nullptr) {}
 	};
 
-	Node *first, *last, *curr;
-	int len;
-	
-public:
-	List() : first(nullptr), last(nullptr), curr(nullptr), len(0) {}
-	void add(T data) {
-		Node *newnode = new Node(data, first);
-		first = newnode;
-		if (len==0) last = newnode;
-		len++;
-	}
+	Elem *first, *last;
+	int n;
 
-	void append(T data) {
-		Node *newnode = new Node(data);
-		if (len==0) first = newnode;
-		else last->next = newnode;
-		last = newnode;
-		len++;
-	}
+	void copy(const List& list);
 
-	void begin() { curr = first; }
-	bool exists() { return curr!=nullptr; }
-	void next() { if (exists()) curr = curr->next; }
-	T& get() { return curr->data; }
-	int size() { return len; }
+	List() : first(nullptr), last(nullptr), n(0) {}
+	List(const List& list) { copy(list); }
+	List& operator=(const List& list) { if (this != &list) { erase(); copy(list); } return *this; }
+	virtual ~List() { erase(); }
 
+	int size() const { return n; }
+	bool empty() const { return n == 0; }
+	void erase();
+
+	void pushBack(T data);
+	void pushFront(T data);
+
+ 	class Iterator {
+		List *list;
+		Elem *current, *deleted_next;
+	public:
+		Iterator(List *list, Elem *current) : list(list), current(current), deleted_next(nullptr) {}
+		T operator*() const { return current->data; }
+		void operator++() { current = (current==nullptr)?deleted_next:current->next; deleted_next = nullptr; }
+		void operator++(int k) { ++(*this); }
+		bool exists() const { return current != nullptr; }
+		void remove() {
+			Elem *prev = current->prev;
+			Elem *next = current->next;
+			deleted_next = next;
+
+			if (prev != nullptr)
+				prev->next = next;
+			else
+				list->first = next;
+
+			if (next != nullptr)
+				next->prev = prev;
+			else
+				list->last = prev;
+
+			delete current;
+			current = nullptr;
+			list->n--;
+		}
+	};
+
+	Iterator begin() { return Iterator(this, first); }
 };
+
+template<class T>
+void List<T>::copy(const List<T> & list)
+{
+	for (Elem *it = list.first; it != nullptr; it = it->next)
+		pushBack(it->data);
+}
+
+template<class T>
+void List<T>::erase()
+{
+	Elem *it = first;
+	while (it != nullptr) {
+		Elem *it_next = it->next;
+		delete it;
+		it = it_next;
+	}
+
+	first = last = nullptr;
+	n = 0;
+}
+
+template<class T>
+void List<T>::pushBack(T data)
+{
+	Elem *newelem = new Elem(data);
+	newelem->prev = last;
+	(empty())?first:last->next = newelem;
+	last = newelem;
+	n++;
+}
+
+template<class T>
+void List<T>::pushFront(T data)
+{
+	Elem *newelem = new Elem(data);
+	newelem->next = first;
+	(empty())?last:first->prev = newelem;
+	first = newelem;
+	n++;
+}
 #endif
